fd8dea35d98246941b38820548d28fcee3467bd2
Noyan Alimov <noyan.alimov@gmail.com>
2020-02-16 13:57:54 +0000

    worked on entity extraction component

--- src/components/EntityExtraction.js
+++ src/components/EntityExtraction.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 const EntityExtraction = () => {
   return(
-    <div>
+    <div className="tc">
+      <p className='f4 pa2 ma2'>
+        text: ACME corp was founded by John Smith in Chicago<br/>
+        language: en<br/>
+        entities:<br/>
+          location:<br/>
+            Chicago
+
+          keyword":<br/>
+            John,
+            corp,
+            Smith,
+            Chicago,
+            ACME
+
+          organization:<br/>
+            ACME
+
+          person:<br/>
+            John Smith
+      </p>
     </div>
   );
 }
